ab1f8c43334ca19cb11ababc63851809dd93fe33
Victor Minden <vminden@flatironinstitute.org>
2017-09-19 13:04:05 -0400

    added skeleton file

---
+++ online_pca_simulations.py
@@ -0,0 +1,12 @@
+# Title: online_pca_simulations.py
+# Description: A function for testing an online PCA algorithm
+# Author: Victor Minden (vminden@flatironinstitute.org)
+# Notes: Adapted from code by Andrea Giovanni
+# Reference: None
+
+##############################
+# Imports
+import numpy as np
+from scipy.linalg import solve as solve
+import util
+##############################
