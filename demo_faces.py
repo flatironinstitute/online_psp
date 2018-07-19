0d0b6399d027794903efd94749e1c52a75214b67
agiovann <andrea.giovannucci@gmail.com>
2018-07-19 15:47:17 -0400

    demofaces working with all files

--- demo_faces.py
+++ demo_faces.py
@@ -3,7 +3,7 @@
 # Author: Victor Minden (vminden@flatironinstitute.org)and Andrea Giovannucci (agiovannucci@flatironinstitute.org)
 # Notes: Adapted from code by Andrea Giovannucci and Cengiz Pehlevan
 # Reference: (Cardot and Degras, 2015)
-#%%
+
 ##############################
 from incremental_pca import IncrementalPCA_CLASS
 from ccipca import CCIPCA_CLASS
@@ -11,54 +11,50 @@ from if_minimax_subspace_projection import IF_minimax_PCA_CLASS
 import numpy as np
 import pylab as pl
 import time
-from util import subspace_error
-#%% GENERATE TEST DATA
-# Parameters
-#%%
-ld = loadmat('./datasets/ATT_faces_112_92.mat')
-fea = ld['fea']
-gnd = ld['gnd']
+from util import subspace_error, load_dataset
 
-#%%
-X = fea.astype(np.float)
-X -= X.mean(0)[None,:]
+for dset in ['ATT_faces_112_92.mat','ORL_32x32.mat','YaleB_32x32.mat']:
+    pl.figure()
+    #%% GENERATE TEST DATA
+    X, U, sigma2 = load_dataset(dataset_name='./datasets/'+dset, return_U=True, q=None)
 
-#%%
-n_epoch = 1
-d, q, n = X.shape[0], 20, X.shape[1]
-X, U, sigma2 = generate_samples(d, q, n)
-lambda_1 = np.random.normal(0, 1, (q,)) / np.sqrt(q)
-# Parameters IF_minimax_PCA_CLASS
-tau = 0.5
-# Simulation parameters
-compute_error = True
-#%% RUN ALGORITHMS
-errs = []
-Uhat0 = X[:, :q]/(X[:, :q]**2).sum(0)
-ipca = IncrementalPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1)
-if_mm_pca = IF_minimax_PCA_CLASS(q, d, W0=Uhat0.T, Minv0=None, tau=tau)
-ccipca = CCIPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1, cython='auto', in_place=False)
-algorithms = {'ipca':ipca, 'if_mm_pca':if_mm_pca, 'ccipca':ccipca}
+    #%%
+    n_epoch = 1
+    d, q, n = X.shape[0], 20, X.shape[1]
+    lambda_1 = np.random.normal(0, 1, (q,)) / np.sqrt(q)
+    # Parameters IF_minimax_PCA_CLASS
+    tau = 0.5
+    # Simulation parameters
+    compute_error = True
 
-times = dict()
-errs = dict()
-for name, algo in algorithms.items():
-    err = []
-    time_1 = time.time()
-    for n_e in range(n_epoch):
-        for x in X.T:
-            algo.fit_next(x)
-            err.append(subspace_error(algo.get_components(), U[:, :q]))
-    time_2 = time.time() - time_1
-    errs[name] = err
-    times[name] = time_2
-#%% DISPLAY RESULTS
-for name in algorithms.keys():
-    pl.semilogy(errs[name])
-    pl.xlabel('relative subspace error')
-    pl.xlabel('samples')
-    print('Elapsed time ' + name +':' + str(times[name]))
-    print('Final subspace error ' + name +':' + str(subspace_error(np.asarray(ipca.Uhat), U[:, :q])))
+    #%% RUN ALGORITHMS
+    errs = []
+    Uhat0 = X[:, :q]/(X[:, :q]**2).sum(0)
+    ipca = IncrementalPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1)
+    if_mm_pca = IF_minimax_PCA_CLASS(q, d, W0=Uhat0.T, Minv0=None, tau=tau)
+    ccipca = CCIPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1, cython='auto', in_place=False)
+    algorithms = {'ipca':ipca, 'if_mm_pca':if_mm_pca, 'ccipca':ccipca}
 
-pl.legend(algorithms.keys())
-pl.show()
+    times = dict()
+    errs = dict()
+    for name, algo in algorithms.items():
+        err = []
+        time_1 = time.time()
+        for n_e in range(n_epoch):
+            for x in X.T:
+                algo.fit_next(x)
+                err.append(subspace_error(algo.get_components(), U[:, :q]))
+        time_2 = time.time() - time_1
+        errs[name] = err
+        times[name] = time_2
+    #%% DISPLAY RESULTS
+    for name in algorithms.keys():
+        pl.semilogy(errs[name])
+        pl.xlabel('relative subspace error')
+        pl.xlabel('samples')
+        print('Elapsed time ' + name +':' + str(times[name]))
+        print('Final subspace error ' + name +':' + str(subspace_error(np.asarray(ipca.Uhat), U[:, :q])))
+
+    pl.legend(algorithms.keys())
+    # pl.show()
+    pl.savefig(dset[:-3]+'pdf')
