8fe5434d9be5a96519ed5671c6078842de52571c
Victor Minden <victorminden@gmail.com>
2018-07-20 16:05:36 -0400

    a seemingly working version of letipca (no cython though, yet)

--- letipca.py
+++ letipca.py
@@ -1,4 +1,5 @@
-# Title: ccipca.py
+# Title: letipca.py
+# TODO: update me
 # Description: A function for PCA using the Candid Covariance-Free Incremental PCA approach
 # Author: Victor Minden (vminden@flatironinstitute.org) and Andrea Giovannucci (agiovannucci@flatironinstitute.org)
 # Notes: Adapted from code by Andrea Giovannucci and Cengiz Pehlevan
@@ -13,13 +14,12 @@ from scipy.linalg import solve as solve
 import util
 from util import subspace_error
 import time
-import coord_update
 
 ##############################
 
 
 
-class CCIPCA_CLASS:
+class LETIPCA_CLASS:
     """
     Parameters:
     ====================
@@ -28,26 +28,13 @@ class CCIPCA_CLASS:
     Uhat0         -- Initial guess for the eigenspace matrix U, must be of size d-by-q
     lambda0       -- Initial guess for the eigenvalues vector lambda_, must be of size q
     ell           -- Amnesiac parameter (see reference)
-    cython: True, False or 'auto'
-        whether to use computationally optimized cython function
 
     Methods:
     ====================
-    fit()
     fit_next()
     """
 
-    def __init__(self, q, d, Uhat0=None, lambda0=None, ell=2, cython=True, in_place=False,verbose =False):
-        #        if d>=2000 and cython:
-        #            raise Exception('Cython Code is Limited to a 2000 dimensions array: use cython=False')
-        if cython == 'auto':
-            if d>1000:
-                cython = False
-            else:
-                cython = True
-        if verbose:
-            print('Using Cython:' + str(cython))
-
+    def __init__(self, q, d, Uhat0=None, lambda0=None, ell=2,in_place=False,verbose=False):
         if Uhat0 is not None:
             assert Uhat0.shape == (d, q), "The shape of the initial guess Uhat0 must be (d,q)=(%d,%d)" % (d, q)
             self.Uhat = Uhat0.copy()
@@ -62,19 +49,14 @@ class CCIPCA_CLASS:
             assert lambda0.shape == (q,), "The shape of the initial guess lambda0 must be (q,)=(%d,)" % (q)
             self.lambda_ = lambda0.copy()
         else:
-            self.lambda_ = np.abs(np.random.normal(0, 1, (q,)) / np.sqrt(q))
+            self.lambda_ = np.abs(np.random.normal(0, 1, (q,))) / np.sqrt(q)
 
         self.q = q
         self.d = d
         self.ell = ell
-        self.cython = cython
-        self.v = np.zeros(d)
         self.in_place = in_place
-
-    def fit(self, X):
-        self.Uhat, self.lambda_ = coord_update.coord_update_total(X, X.shape[-1], self.d, np.double(self.t),
-                                                                  np.double(self.ell), self.lambda_, self.Uhat, self.q,
-                                                                  self.v)
+        self.tol = 1e-12
+        self.max_its = 30
 
 
     def fit_next(self, x_):
@@ -85,23 +67,38 @@ class CCIPCA_CLASS:
 
         assert x.shape == (self.d,)
 
-        if self.cython:
-            self.Uhat, self.lambda_ = coord_update.coord_update(x, self.d, np.double(self.t), np.double(self.ell),
-                                                                self.lambda_, self.Uhat, self.q, self.v)
-        else:
-            t, ell, lambda_, Uhat, q = self.t, self.ell, self.lambda_, self.Uhat, self.q
-            old_wt = max(1,t-ell) / (t+1)
-            for i in range(q):
-              # TODO: is the max okay?
-                v = old_wt * lambda_[i] * Uhat[:, i] + (1-old_wt) * np.dot(x, Uhat[:,i]) * x
-                lambda_[i] = np.sqrt(v.dot(v))  # np.linalg.norm(v)
-                Uhat[:, i] = v / lambda_[i]
-                x = x - np.dot(x, Uhat[:, i]) * Uhat[:, i]
+        t, ell, lambda_, Uhat, q = self.t, self.ell, self.lambda_, self.Uhat, self.q
+        tol, max_its = self.tol, self.max_its
+        old_wt = max(1,t-ell) / (t+1)
+        Uhat_new   = Uhat.copy()
+        lambda_new = lambda_.copy()
+        for i in range(q):
+            v_old = np.zeros_like(Uhat_new[:, i]) + 100
+            for _ in range(max_its):
+                Uhat_new[:, i] /= lambda_new[i]
+                if np.linalg.norm(v_old-Uhat_new[:, i]) < tol:
+                    break
+                old_term  = lambda_[i] * Uhat[:,i]
+                data_term = np.dot(x, Uhat_new[:, i]) * x
+                if i>0:
+                    term2 = lambda_new[:i] * \
+                        np.dot(Uhat_new[:, :i].T, Uhat_new[:, i])
+                    term2 = Uhat_new[:,:i].dot(term2)
 
-            self.Uhat = Uhat
-            self.lambda_ = lambda_
+                    term1 = lambda_[:i] * np.dot(Uhat[:, :i].T, Uhat_new[:, i])
+                    term1 = Uhat[:,:i].dot(term1)
+                else:
+                    term1 = 0
+                    term2 = 0
+
+                v_old = Uhat_new[:, i]
+                Uhat_new[:, i] = old_wt * term1 - term2 + \
+                    (1-old_wt) * data_term + old_wt * old_term
+                lambda_new[i] = np.linalg.norm(Uhat_new[:,i])
+
+        self.Uhat = Uhat_new.copy()
+        self.lambda_ = lambda_new
         self.t += 1
-
 
     def get_components(self, orthogonalize=True):
         '''
@@ -121,32 +118,36 @@ class CCIPCA_CLASS:
 
         return components
 
+
 #%%
 if __name__ == "__main__":
     # %%
-    print('Testing CCIPCA')
+    print('Testing LETIPCA')
     from util import generate_samples
     import pylab as pl
     # Parameters
     n_epoch = 1
     d, q, n = 20, 5, 1000
     X, U, sigma2 = generate_samples(d, q, n)
-    lambda_1 = np.random.normal(0, 1, (q,)) / np.sqrt(q)
+    lambda_1 = np.abs(np.random.normal(0, 1, (q,))) / np.sqrt(q)
     Uhat0 = X[:, :q] / np.sqrt((X[:, :q] ** 2).sum(0))
     # %%
     errs = []
-    ccipca = CCIPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1, cython='auto', in_place=False)
+    letipca = LETIPCA_CLASS(q, d, Uhat0=Uhat0, lambda0=lambda_1, in_place=False)
     time_1 = time.time()
     for n_e in range(n_epoch):
         for x in X.T:
-            ccipca.fit_next(x)
-            errs.append(subspace_error(ccipca.get_components(), U[:,:q]))
+            letipca.fit_next(x)
+            errs.append(subspace_error(
+                letipca.get_components(), U[:, :q]))
     time_2 = time.time() - time_1
+
     pl.semilogy(errs)
     pl.xlabel('relative subspace error')
     pl.xlabel('samples')
     print('Elapsed time:' + str(time_2))
-    print('Final subspace error:' + str(subspace_error(ccipca.get_components(), U[:, :q])))
+    print('Final subspace error:' +
+          str(subspace_error(letipca.get_components(), U[:, :q])))
     pl.show()
     pl.pause(3)
 
