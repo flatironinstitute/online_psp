73e4b4eb3904737a44de4d752b320305d0e1243f
agiovann <andrea.giovannucci@gmail.com>
2018-07-26 17:29:20 -0400

    working on ex_real_data_errors

--- ex_real_data_errors.py
+++ ex_real_data_errors.py
@@ -20,7 +20,7 @@ matplotlib.rcParams['ps.fonttype'] = 42
 # general parameters
 
 error_options = {
-    'n_skip': 20,
+    'n_skip': 64,
     'orthogonalize_iterate': False,
     'compute_batch_error': True,
     'compute_population_error': True,
@@ -31,26 +31,28 @@ error_options = {
 generator_options = {
     'method': 'real_data',
     'scale_data': True,
-    'shuffle' : True
+    'shuffle' : False
 }
 
 simulation_options = {
         'd': None,
-        'q': 50,
+        'q': 16,
         'n': 'auto',  # can set a number here, will select frames multiple times
         'n0': 0,
         'error_options': error_options,
         'pca_init': False,
         'init_ortho': True,
-        'n_epoch' : 10
+        'n_epoch' : 50
 }
 
 algos = ['if_minimax_PCA', 'incremental_PCA', 'CCIPCA']
+algos = ['if_minimax_PCA', 'CCIPCA']
+
 algo = algos[0]
 algorithm_options = {
     'pca_algorithm': algo,
     'tau': 0.5,
-    'tol': 1e-7
+    'tol': 1e-7,
 }
 
 
@@ -150,26 +152,28 @@ if test_mode == 'illustrative_examples':
     data_fold = os.path.abspath('./real_data_4_examples')
     #redundant but there for flexibility
     names = ['ORL_32x32.mat','YaleB_32x32.mat','ATT_faces_112_92.mat', 'MNIST.mat'][:-3]
-    qs = [8, 16, 32, 64]
+    qs = [16]
     colors = ['b', 'r', 'g']
     n_repetitions = 1
     simulation_options['n'] = 'auto'
     plot = not parallelize
     if rerun_simulation:
         os.mkdir(data_fold)
-    counter = 0
+    counter_q = 0
     all_pars = []
-    for name in names:
-        generator_options['filename'] = './datasets/' + name
+    for q in qs:
 
-        counter += 1
-        if not parallelize:
-            ax = plt.subplot(4, 1, counter)
-        for algo in range(3):
-            algorithm_options['pca_algorithm'] = algos[algo]
-            pop_err_avg = []
-            batch_err_avg = []
-            for q in qs:
+        counter_name = 0
+        for name in names:
+            counter_name += 1
+            generator_options['filename'] = './datasets/' + name
+            if not parallelize:
+                ax = plt.subplot(len(qs), len(names), len(names)*counter_q + counter_name)
+
+            for algo in range(len(algos)):
+                print(name)
+                algorithm_options['pca_algorithm'] = algos[algo]
+                batch_err_avg = []
                 print((name, q))
                 simulation_options['q'] = q
                 all_pars.append(
@@ -177,133 +181,35 @@ if test_mode == 'illustrative_examples':
                      n_repetitions])
 
                 if parallelize:
-                    pop_err_avg = None
                     batch_err_avg = None
                 else:
                     if rerun_simulation:
                         errs_pop, errs_batch = run_test_wrapper(all_pars[-1])
-                        pop_err_avg.append(errs_pop.mean(0)[-1])
-                        batch_err_avg.append(errs_batch.mean(0)[-1])
-                        errs_pop = np.array(errs_pop)
-                        errs_batch = np.array(errs_batch)
+                        batch_err_avg.append(errs_batch.mean(0))
+                        batch_err_avg = np.array(batch_err_avg)
                     else:
                         fname = os.path.join(data_fold, '__'.join(
                             ['fname',name, 'q', str(q), 'algo', algos[algo]]) + '.npz')
                         # fname = os.path.join(data_fold, '__'.join(
                         #     ['rho',str(rho), 'd', str(d), 'q', str(q), 'algo', algos[algo]]) + '.npz')
                         with np.load(fname) as ld:
-                            pop_err_avg.append(np.mean(ld['batch_err'][()], 0)[-1])
-                            batch_err_avg.append(np.mean(ld['population_err'][()], 0)[-1])
+                            batch_err_avg.append(np.mean(ld['population_err'][()], 0))
 
-            if pop_err_avg is not None:
-                plt.title('k=' + str(q))
-                line_pop, = ax.loglog(qs, pop_err_avg, '.-' + colors[algo])
-                line_bat, = ax.loglog(qs, batch_err_avg, '-.' + colors[algo])
-                line_pop.set_label(algos[algo] + '_pop')
-                line_bat.set_label(algos[algo] + '_batch')
-                plt.ylabel('projection error')
-                plt.pause(.1)
+                if pop_err_avg is not None:
+                    plt.title('k=' + str(q))
+                    line_bat, = ax.loglog(batch_err_avg.T, colors[algo])
+                    line_bat.set_label(algos[algo] + '_batch')
+                    plt.ylabel('subspace error')
+                    plt.pause(.1)
+        counter_q += 1
 
-    if pop_err_avg is not None:
+    if batch_err_avg is not None:
         ax.legend()
         plt.xlabel('k')
         plt.pause(3)
 
     if parallelize:
         all_res = dview.map(run_test_wrapper, all_pars)
-
-# %%
-elif test_mode == 'vary_q' or test_mode == 'vary_q_fix_qdata':
-    # %% vary k
-    if test_mode == 'vary_q':
-        data_fold = os.path.abspath('./spiked_cov_vary_k')
-    if test_mode == 'vary_q_fix_qdata':
-        data_fold = os.path.abspath('./spiked_cov_vary_k_fix_qdata')
-        generator_options['q_data'] = 128
-
-    n_repetitions = 15
-    simulation_options['n'] = 3000
-    d_q_params = [(1024, 2), (1024, 8), (1024, 32), (1024, 64), (1024, 128)]
-
-    if rerun_simulation:
-        os.mkdir(data_fold)
-
-    all_pars = []
-    counter = 0
-    for d, q in d_q_params:
-
-        simulation_options['d'] = d
-        simulation_options['q'] = q
-        for algo in range(3):
-            algorithm_options['pca_algorithm'] = algos[algo]
-            pop_err_avg = []
-            batch_err_avg = []
-            for rho in rhos:
-                generator_options['rho'] = rho
-                print((d, q, rho))
-                all_pars.append(
-                    [generator_options.copy(), simulation_options.copy(), algorithm_options.copy(), data_fold,
-                     n_repetitions])
-                if parallelize:
-                    pop_err_avg = None
-                    batch_err_avg = None
-                else:
-                    if rerun_simulation:
-                        errs_pop, errs_batch = run_test_wrapper(all_pars[-1])
-                        pop_err_avg.append(errs_pop.mean(0)[-1])
-                        batch_err_avg.append(errs_batch.mean(0)[-1])
-                        errs_pop = np.array(errs_pop)
-                        errs_batch = np.array(errs_batch)
-                    else:
-                        fname = os.path.join(data_fold, '__'.join(
-                            ['rho', "{:.6f}".format(rho), 'd', str(d), 'q', str(q), 'algo', algos[algo]]) + '.npz')
-
-                        with np.load(fname) as ld:
-                            print(pop_err_avg)
-                            pop_err_avg.append(np.mean(ld['batch_err'][()], 0)[-1])
-                            batch_err_avg.append(np.mean(ld['population_err'][()], 0)[-1])
-
-            if pop_err_avg is not None:
-                ax = plt.subplot(5, 2, 2 * counter + 1)
-                if algo ==0:
-                    line_pop, = ax.loglog(rhos, pop_err_avg, 'o-')
-                else:
-                    line_pop, = ax.loglog(rhos, pop_err_avg,'.-')
-
-                line_pop.set_label('algo=' + algos[algo])
-
-                ax = plt.subplot(5, 2, 2 * counter + 2)
-                if algo == 0:
-                    line_bat, = ax.loglog(rhos, batch_err_avg, 'o-')
-                else:
-                    line_bat, = ax.loglog(rhos, batch_err_avg,'.-')
-                line_bat.set_label('algo=' + algos[algo])
-
-                plt.pause(.1)
-
-        counter += 1
-
-
-    if pop_err_avg is not None:
-        for counter in range(5):
-            ax = plt.subplot(5, 2, 2 * counter + 1)
-            plt.ylabel('population error')
-            plt.xlabel('rho')
-            plt.title('q=' + str(d_q_params[counter][-1]))
-
-            ax = plt.subplot(5, 2, 2 * counter + 2)
-            plt.ylabel('batch error')
-            plt.xlabel('rho')
-            plt.title('q=' + str(d_q_params[counter][-1]))
-
-            plt.pause(2)
-
-
-        ax = plt.subplot(5, 2, 2 * counter + 2)
-        ax.legend()
-
-    if parallelize:
-        all_errs = dview.map(run_test_wrapper, all_pars)
 
 # %% stop cluster
 if parallelize:
